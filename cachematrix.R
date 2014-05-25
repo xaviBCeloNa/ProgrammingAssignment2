2e3233a4e838f94cc3fbb501fc2bd08637e66747
xaviBCeloNa <xvfont@gmail.com>
2014-05-25 23:40:20 +0200

    further comments
    Another description to the Caching Inverse Matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix is a function that creates a "matrix" which is a list containing functions (also an R objects)
+## to some set's and get's. Basically to the Matrix and to the operation used to save computation time matrix Inverse.
+## The way the makeCacheMatrix has been written follows the same makeVector approach.
+
+## cacheSolve computes the inverse of the "matrix" object created with makeCacheMatrix. It first check if the inverse exist. In that
+## case it gets the inverse and skips the costly matrix inversion operation. In case there is no inverse it computes the inverse
+## using Choleski decomposition and sets the inverse in the cache through setInv function.
+
 ## Write a short comment describing this function
 ## The idea behind this function goes in the same direction as the one used as an example (makeVector) 
-
 makeCacheMatrix <- function(x = matrix()) {
   ## Here we define the set's and get's to the matrix and to the inverse
   ## in the same way as with the mean vector example
